b89c27cfdf8568e4f72f4c36235c0efbfbf99b71
Van Tri <vtri2016@gmail.com>
2023-06-23 20:33:45 +0700

    change cicd
    Signed-off-by: Van Tri <vtri2016@gmail.com>

--- data-source.ts
+++ data-source.ts
@@ -44,6 +44,9 @@ switch (process.env.NODE_ENV) {
       synchronize: false,
       migrationsRun: true,
       logging: false,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     };
     break;
 
